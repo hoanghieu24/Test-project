532a5d725936e2cf20deb88d2e9cd014b7746507
Hoang Van Hieu <hvan8677@gmail.com>
2024-06-25 19:26:48 +0700

    HTML

---
+++ Projecthtml6-7-8/Buoi1.js
@@ -0,0 +1,8 @@
+console.log('Hello Hoang Hieus ')
+let a = {
+    name : 'hoang hieu ' , 
+    address : ' nghe an ',
+    age : '20'
+};
+console.log('May ten gi ? ' , `toi ten la `, a.name)
+console.log('May o dau ? ' , ' tao song o ' , a.address)
